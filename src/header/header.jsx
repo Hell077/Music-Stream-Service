3c97c933c88c1a615f6df299b7b33ebbc91dc1ff
Alexey Ovchinnikov <gdhald4@gmail.com>
2024-04-30 22:31:04 +0500

    init repo

---
+++ src/header/header.jsx
@@ -0,0 +1,12 @@
+import style from './header.module.css'
+
+function Header() {
+    return ( 
+        <header>
+            <div className={style.player}></div>
+
+        </header>
+     );
+}
+
+export default Header;
